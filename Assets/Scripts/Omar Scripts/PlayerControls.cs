9ba66ae648eb66c90154e384a621ef57bf402be6
John Blanch <DarkestDinovr@gmail.com>
2022-10-25 12:57:57 -0400

    Array for the buttons

--- Assets/Scripts/Omar Scripts/PlayerControls.cs
+++ Assets/Scripts/Omar Scripts/PlayerControls.cs
@@ -99,40 +99,20 @@ public class PlayerControls : MonoBehaviour
         {
             //Debug.Log(hit.collider.gameObject.name);
             //SwitchButton();
-            Button pressButtonHit = hit.collider.GetComponent<Button>();
-            ButtonUp pressUpHit = hit.collider.GetComponent<ButtonUp>();
+            ButtonTwoDoor pressButtonHit = hit.collider.GetComponent<ButtonTwoDoor>();
+            ButtonOneDoor pressOneHit = hit.collider.GetComponent<ButtonOneDoor>();
             //maze 2 buttons
-            ButtonUpM2 pressUpHitM2 = hit.collider.GetComponent<ButtonUpM2>();
-            ButtonUp1 pressUpHit1 = hit.collider.GetComponent<ButtonUp1>();
-            ButtonUp2 pressUpHit2 = hit.collider.GetComponent<ButtonUp2>();
-            ButtonUp3 pressUpHit3 = hit.collider.GetComponent<ButtonUp3>();
             ButtonRotate pressRotateHit = hit.collider.GetComponent<ButtonRotate>();
             //Button pressButton = hit.collider.GetComponent<Button>();
             if (pressButtonHit != null)
             {
                 pressButtonHit.operate();
             }
-            if (pressUpHit != null)
+            if (pressOneHit != null)
             {
-                pressUpHit.operate();
+                pressOneHit.operate();
             }
             //maze 2 buttons
-            if (pressUpHitM2 != null)
-            {
-                pressUpHitM2.operate();
-            }
-            if (pressUpHit1 != null)
-            {
-                pressUpHit1.operate();
-            }
-            if (pressUpHit2 != null)
-            {
-                pressUpHit2.operate();
-            }
-            if (pressUpHit3 != null)
-            {
-                pressUpHit3.operate();
-            }
             if (pressRotateHit != null)
             {
                 pressRotateHit.operate();
