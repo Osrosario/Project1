f64c4dc4230b292a8d77d040a2f71ce22d8d7edf
John Blanch <DarkestDinovr@gmail.com>
2022-11-07 18:00:44 -0500

    Lighting and room 3

---
+++ Assets/Scripts/RestartOnHit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartOnHit : MonoBehaviour
+{
+    public string restartScene;
+    public bool RestartToggle = false;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(restartScene);
+            Cursor.lockState = CursorLockMode.None;
+        }
+          
+    }
+
+}
